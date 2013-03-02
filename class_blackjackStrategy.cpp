872b106e0fece2cdc22583297756c2a2e951891f
davidsteinsland <david@davidsteinsland.net>
2013-03-02 14:18:13 +0100

    Skriver ut alle kortene til spilleren

--- class_blackjackStrategy.cpp
+++ class_blackjackStrategy.cpp
@@ -14,13 +14,16 @@ blackjackStrategy::blackjackStrategy (controlType type)
 
 action* blackjackStrategy::takeAction (gameState *g)
 {
-	// std::cout << "Jeg returnerer nå en action" << std::endl;
 	blackjackState *state = (blackjackState*)g;
 	std::vector<cards::card> cardsv = state->myCards();
 	std::vector<cards::card> dealer = state->dealersCards();
 	
-	// only one card visible! We choose the first one
-	std::cout << "Du har: " << cardsv[0].name() << std::endl;
+	std::cout << "Du har: ";
+	for (std::vector<cards::card>::iterator it = cardsv.begin(); it < cardsv.end(); it++)
+		std::cout << it->name() << " ";
+	std::cout << std::endl;
+	
+	// we're only allowed to see one of the dealer's cards.
 	std::cout << "Dealer har: " << dealer[0].name() << std::endl;
 	
 	return new blackjackAction (TERMINAL);
